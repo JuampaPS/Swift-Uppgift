f79d253707a095d8ac6ae5cd9d9e251f93c7b2f0
Juan Pablo Palacio Soler <juampa_ps@icloud.com>
2024-11-11 10:05:45 +0100

    Initial Commit

---
+++ Swift Uppgift/Swift_UppgiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UppgiftApp.swift
+//  Swift Uppgift
+//
+//  Created by Pablo Soler on 2024-11-11.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UppgiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift UppgiftTests/Swift_UppgiftTests.swift
@@ -0,0 +1,16 @@
+//
+//  Swift_UppgiftTests.swift
+//  Swift UppgiftTests
+//
+//  Created by Pablo Soler on 2024-11-11.
+//
+
+import Testing
+
+struct Swift_UppgiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
