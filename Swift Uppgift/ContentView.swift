ee0bc41a864c1bc796f12542e193174842fdf182
Juan Pablo Palacio Soler <juampa_ps@icloud.com>
2024-11-14 14:27:39 +0100

    Fix Finish

--- Swift Uppgift/ContentView.swift
+++ Swift Uppgift/ContentView.swift
@@ -9,13 +9,64 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        VStack (spacing: 0){
+            
+                        
+            HStack (spacing: 0.0){
+                
+                VStack{
+                    // RED
+                }
+                .frame(maxWidth: .infinity, maxHeight: 150)
+                .background(Color.red)
+                VStack {
+                    // GUL
+                }
+                .frame(maxWidth: .infinity, maxHeight: 150)
+                .background(Color.yellow)
+                VStack{
+                    // GRÖN
+                }
+                .frame(maxWidth: .infinity, maxHeight: 150)
+                .background(Color.green)
+            }
+            VStack {
+                // GREY
+            }
+            .frame(maxWidth: .infinity, maxHeight:75)
+            .background(Color.gray)
+
+            
+            VStack {
+                // WHITE BOX
+            }
+                .frame(width: 150, height: 150)
+                .background(Color.white)
+            Spacer()
+            
+            
+    
+            HStack  {
+                
+                Spacer()
+                
+                VStack {
+                    
+                }
+                .frame(width: 70, height: 70)
+                .background(Color.black)
+                .padding()
+                
+            }
+                .frame(maxWidth: .infinity, maxHeight:75)
+                .background(Color.red)
+                
+            
+            
         }
-        .padding()
+        .frame(maxHeight: .infinity)
+        .background(Color.blue)
     }
 }
 
